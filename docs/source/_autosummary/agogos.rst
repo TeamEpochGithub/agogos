5fbab44cf49d287b92dc912c03c8bc57d505aba5
schobbejak <jmvanselm@gmail.com>
2024-03-12 21:14:43 +0100

    Add sphinx awesome and logo

---
+++ docs/source/_autosummary/agogos.rst
@@ -0,0 +1,32 @@
+﻿agogos
+======
+
+.. automodule:: agogos
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+.. rubric:: Modules
+
+.. autosummary::
+   :toctree:
+   :recursive:
+
+   agogos.pipeline
+   agogos.training
+   agogos.transforming
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,15 +14,7 @@ copyright = "2024, Jasper van Selm"
 author = "Jasper van Selm"
 
 # Add root path of repository
-sys.path.insert(0, Path("..").resolve().as_posix())
 sys.path.insert(0, Path("../..").resolve().as_posix())
-print(sys.path)
-
-# try:
-#     # import agogos
-
-# except Exception as e:
-#     print("FAUl")
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -40,5 +32,5 @@ source_suffix = {".rst": "restructuredtext", ".md": "markdown"}
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinxawesome_theme"
 html_static_path = ["_static"]
